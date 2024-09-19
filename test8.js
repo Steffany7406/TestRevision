238f9d9f8d5b0cd5afd9ab91384041ec8a0b6dfd
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-19 12:24:37 -0300

    Add files via upload

---
+++ test8.js
@@ -0,0 +1,10 @@
+var count = 0
+do {
+  var num = parseInt(prompt("Digite um número!"))
+  if (num > 100){
+    alert("Você digitou um número maior que 100!!")
+    count++
+  }
+} while (num < 100) {
+  alert("Existem números maiores que 100!")
+}
