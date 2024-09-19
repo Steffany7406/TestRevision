5923fc785b4b26ea4dc96abb6265923a09da736e
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-19 12:32:51 -0300

    test8.js

--- test8.js
+++ test8.js
@@ -8,3 +8,16 @@ do {
 } while (num < 100) {
   alert("Existem números maiores que 100!")
 }
+
+/*
+var count = 0
+var num = 20
+while (count !== num) {
+  if (num > 100){
+    num++
+    console.log("Você digitou um número maior que 100!!")
+  }else {
+    console.log("Epa! Esse é menor que 100!")
+  }
+}
+*/
