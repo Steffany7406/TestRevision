598df64bff0fb67aadebb5fb34d5b9b600866b1e
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-19 13:57:07 -0300

    test12.js

--- test12.js
+++ test12.js
@@ -1,6 +1,6 @@
-let numero;
+let i;
 do {
-    numero = parseFloat(prompt("Insira um número (insira um valor negativo para sair):"));  
-} while (numero >= 0); 
+   var num = parseFloat(prompt("Insira um número (insira um valor negativo para sair):"));  
+} while (num >= 0); 
 
 alert("Você inseriu um número negativo. O loop foi encerrado.");
